deaa18fabaecec556b46b6ba433af0571e21d47c
maria2332 <maria.ballesteros2004@gmail.com>
2023-03-30 08:37:45 +0200

    enunciados

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+"""
+Creación (0,5 puntos)
+Crea una clase llamada Alumno que tenga los atributos nombre y nota
+Crea el constructor de la clase. Añadir en el constructor un print para informar de que el alumno se ha creado con éxito
+Crear un método llamado calificación que imprima por pantalla si el alumno ha aprobado o suspendido en base a su nota
+Experimentación (0,5 Puntos)
+Crea algunos alumnos
+Prueba a ejecutar el método calificación de cada objeto que has creado
+"""
