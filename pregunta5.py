060ec28aa1b71393431fb864c6ce4086eb3fdf97
maria2332 <maria.ballesteros2004@gmail.com>
2023-03-30 09:54:34 +0200

    terminado

--- pregunta5.py
+++ pregunta5.py
@@ -5,37 +5,40 @@ La alianza rebelde necesita comunicarse de manera segura pero el imperio galáct
  se deberá generar dos tablas hash para encriptar y desencriptar, para los caracteres desde el “ ” hasta el “}” –es decir desde el 32 al 125 de la tabla ASCII.
  """
 
-
-class Encriptacion:
-    def __init__(self, texto):
-        self.texto = texto
+class Encriptar:
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
         self.tabla_encriptar = {}
         self.tabla_desencriptar = {}
-        self.encriptado = ""
-        self.desencriptado = ""
+        self.mensaje_encriptado = ""
+        self.mensaje_desencriptado = ""
         self.generar_tablas()
+        self.encriptar()
+        self.desencriptar()
 
     def generar_tablas(self):
         for i in range(32, 126):
-            self.tabla_encriptar[chr(i)] = chr(i + 8)
-            self.tabla_desencriptar[chr(i + 8)] = chr(i)
+            self.tabla_encriptar[chr(i)] = self.generar_caracter_encriptado(chr(i))
+            self.tabla_desencriptar[self.tabla_encriptar[chr(i)]] = chr(i)
+
+    def generar_caracter_encriptado(self, caracter):
+        caracter_encriptado = ""
+        for i in range(8):
+            caracter_encriptado += chr(ord(caracter) + i)
+        return caracter_encriptado
 
     def encriptar(self):
-        for i in self.texto:
-            self.encriptado += self.tabla_encriptar[i]
-        return self.encriptado
+        for caracter in self.mensaje:
+            self.mensaje_encriptado += self.tabla_encriptar[caracter]
 
     def desencriptar(self):
-        for i in self.encriptado:
-            self.desencriptado += self.tabla_desencriptar[i]
-        return self.desencriptado
+        for i in range(0, len(self.mensaje_encriptado), 8):
+            self.mensaje_desencriptado += self.tabla_desencriptar[self.mensaje_encriptado[i:i+8]]
 
     def __str__(self):
-        return f"Texto encriptado: {self.encriptado} Texto desencriptado: {self.desencriptado}"
-
-if __name__ == "__main__": 
-    texto = "Hola mundo"
-    encriptacion = Encriptacion(texto)
-    print(encriptacion.encriptar())
-    print(encriptacion.desencriptar())
-    print(encriptacion)
+        return f"Mensaje original: {self.mensaje} \nMensaje encriptado: {self.mensaje_encriptado} \nMensaje desencriptado: {self.mensaje_desencriptado}" 
+    
+if __name__ == "__main__":
+    mensaje = "Hola mundo"
+    encriptar = Encriptar(mensaje)
+    print(encriptar) 
--- pregunta6.py
+++ pregunta6.py
@@ -4,5 +4,10 @@ Realiza el  código para calcular el determinante de una matriz cuadrada de [5 x
 
 #Sarrus solo se puede usar en matrices de 3x3, por lo tanto, para matrices de 5x5 necesitaremos descomponer la matriz en submatrices de 3x3 y aplicar la regla de Sarrus a cada una de ellas.
 
+class Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
 
-
+    def __str__(self):
+        return f"{self.dato}"
